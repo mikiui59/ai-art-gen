a8d0a0159822160f5bbee412d80230e126a91ef7
mikiui59 <54744152+mikiui59@users.noreply.github.com>
2022-05-20 11:04:24 +0200

    Update gen.py

--- gen.py
+++ gen.py
@@ -9,15 +9,6 @@ from keras.backend import clear_session
 import gc
 import subprocess
 from tqdm import tqdm
-text_one=["A painting of a", "A pencil art sketch of a", "An illustration of a", "A photograph of a"]
-text_two=["spinning","dreaming","watering","loving","sleeping","repeating","surreal","psychedelic"]
-text_three=["fish","cat","horse","dog","house","door","table","tree","grass", "flower", "plant","bloom", "spanner","spider", "figurine", "statue", "car",  "monitor"]
-styles=["Art Nouveau", "Camille Pissarro", "Claude Monet", "Fauvism", "Futurism", "Impressionism",
- "Picasso", "Pop Art", "Modern art", "Surreal Art", "Sandro Botticelli", "oil paints", "watercolours", "weird bananas", "strange colours",'Baroque','Abstract Expressionism','Classicism','Expressionism','Fauvism','Impressionism','Neo Impressionism','Performance Art','Pop Art','Post Impressionism']
-place=['forest','sky','space','moon','ice','fire']
-t=['in','on','at','into','under']
-text_new=["fish","cat","horse","dog","house","table","tree","grass", "plant","bloom", "spanner","spider", "figurine", "statue", "car",  "monitor"]
-
 
 with open('text.txt','r') as f:
   text=f.read()
