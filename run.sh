5b8a2e0e8d5dcc6a59afc89e4ff815c87b894638
mikiui59 <54744152+mikiui59@users.noreply.github.com>
2022-05-20 13:33:14 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,3 +11,4 @@ mkdir checkpoints
 curl -L -o checkpoints/vqgan_imagenet_f16_16384.yaml -C - "https://heibox.uni-heidelberg.de/d/a7530b09fed84f80a887/files/?p=%2Fconfigs%2Fmodel.yaml&dl=1"
 curl -L -o checkpoints/vqgan_imagenet_f16_16384.ckpt -C - "https://heibox.uni-heidelberg.de/d/a7530b09fed84f80a887/files/?p=%2Fckpts%2Flast.ckpt&dl=1"
 
+cd ../
